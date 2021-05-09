473ca162fa601ab24d5ba213efdbf9acdca0fdc1
Akhilraj Nambiar <nambiarakhilraj01@gmail.com>
2021-05-09 12:38:31 +0530

    Created a basic book skeleton

--- main.js
+++ main.js
@@ -1,72 +1,48 @@
 function main(){
-        const canvas = document.querySelector('#c');
-        const renderer = new THREE.WebGLRenderer({canvas});
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
-        const fov = 75;
-        const aspect = 2;
-        const near = 0.1;
-        const far = 100;
-        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        camera.position.z = 2;
+  const scene = new THREE.Scene();
 
-        const scene = new THREE.Scene();
+  const fov = 75;
+  const aspect = 2;
+  const near = 0.1;
+  const far = 1000;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 12;
 
-        {
-          const color = 0xffffff;
-          const intensity = 1;
-          const lighting = new THREE.DirectionalLight(color, intensity);
-          lighting.position.set(-1,2,4);
-          scene.add(lighting)
-        }
+  const boxWidth = 3;
+  const boxHeight = 5;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-        const boxWidth = 1;
-        const boxHeight = 1;
-        const boxDepth = 1;
-        const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-        
-        function makeCubes(geometry, color, x){
-          const material = new THREE.MeshPhongMaterial({color});
-          const cube = new THREE.Mesh(geometry, material);
-          scene.add(cube);
-          cube.position.x = x;
-          return cube;
-        }
-        const cubes=[
-          makeCubes(geometry, 0x0000ff, 0),
-          makeCubes(geometry, 0x00ff00, -2),
-          makeCubes(geometry, 0xff0000, 2)
-        ];
+  const material = new THREE.MeshBasicMaterial({color:0xff0000});
 
-        function resizeRendererToDisplaySize(renderer){
-          const pixelRatio = window.devicePixelRatio;
-          const height = canvas.clientHeight * pixelRatio | 0;
-          const width = canvas.clientWidth * pixelRatio | 0;
-          const needResize = canvas.width !== width || canvas.height !== height;
-          if(needResize){
-            renderer.setSize(width, height, false);
-          }
-          return needResize;
-        }
+  const book = new THREE.Mesh(geometry, material);
 
-        function render_rotate(time){
-          time *= 0.001;
+  function resizeRenderToDisplay(renderer){
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if(needResize){
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
 
-          if(resizeRendererToDisplaySize(renderer)){
-          // const canvas = renderer.domElement();
-          camera.aspect = canvas.clientWidth/ canvas.clientHeight;
-          camera.updateProjectionMatrix();
-          }
+  if(resizeRenderToDisplay(renderer)){
+    camera.aspect = canvas.clientWidth/canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
 
-          cubes.forEach((cube, change)=>{
-            const speed = 1 + change*(0.1);
-            const rot = time * speed;
-            cube.rotation.x = rot;
-            cube.rotation.y = rot;
-          });
-          renderer.render(scene, camera);
-          requestAnimationFrame(render_rotate);
-        }
-        requestAnimationFrame(render_rotate);
-      }
-
-  main();
+  scene.add(book);
+  function render_rotate(time){
+    time *= 0.001;
+    book.rotation.x = time;
+    book.rotation.y = time;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render_rotate);
+  }
+  requestAnimationFrame(render_rotate);
+}
+main();
